73ea604c42d11e57a53f1adc40ff626f340d772d
landriesnidis <landriesnidis@yeah.net>
2018-01-01 23:45:08 +0800

    创建了控制器工具类，用来辅助页面临时跳转与动态信息的注入

---
+++ src/main/java/pers/landriesnidis/ecc/controller/ControllerUtils.java
@@ -0,0 +1,66 @@
+package pers.landriesnidis.ecc.controller;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.ui.ModelMap;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+
+/**
+ * Created by landriesnidis on 18-1-1.
+ */
+public class ControllerUtils {
+
+    /**
+     * 检测Session是否有效
+     * @param map
+     * @param request
+     * @return
+     */
+    public static String checkSessionInvalid(ModelMap map, HttpServletRequest request){
+        //检查是否显示错误提示信息
+        Object account = request.getSession().getAttribute("account");
+        if(account==null){
+            request.getSession().setAttribute("returnUrl",request.getRequestURI());
+            return "redirect:/loginPage";
+        }else{
+            map.addAttribute("account",account.toString());
+            return null;
+        }
+
+
+    }
+
+    /**
+     * 根据异常信息获取跳转链接
+     * @param map
+     * @param request
+     * @param e
+     * @return
+     */
+    public static String getRedirectUrlByException(ModelMap map, HttpServletRequest request, DataAccessException e){
+
+        SQLException exception = (SQLException)e.getCause();
+
+        switch(exception.getMessage()){
+
+            case "IDENTITY_INVALID":
+                request.getSession().setAttribute("error_info","身份已失效，请重新登录。");
+                request.getSession().setAttribute("returnUrl",request.getRequestURI());
+                return "redirect:/loginPage";
+
+            case "AUTHENTICATION_FAILED":
+                request.getSession().setAttribute("error_info","帐号或密码错误，请再次确认。");
+                return "redirect:/loginPage";
+
+            case "PASSWORD_IS_WRONG":
+                request.getSession().setAttribute("error_info","原密码不正确。");
+                return "redirect:/userManage";
+
+            case "EMAIL_OCCUPIED":
+                request.getSession().setAttribute("error_info","该Email已经被注册过。");
+                return "redirect:/registerPage";
+        }
+        return null;
+    }
+}
